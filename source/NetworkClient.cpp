cee5bda7ee6378850a7e3179861c2eabc05cb2a8
BerryRock0 <mdenials128@gmail.com>
2026-09-20 21:43:38 +0000

    Update NetworkClient.cpp

--- source/NetworkClient.cpp
+++ source/NetworkClient.cpp
@@ -1,4 +1,40 @@
+#include "NetworkClient.h"
+
+#include <utility>
+
 // NetworkClient.cpp
+NetworkClient::NetworkClient(SnapshotHandler handler) : snapshotHandler(std::move(handler)){}
+
+bool NetworkClient::Connect(const std::string &host,    uint16_t port)
+{  
+	connected = true;
+    return connected;
+}
+
+void NetworkClient::Disconnect()
+{
+	connected = false;
+}
+
+void NetworkClient::Poll()
+{  
+	if(!connected)
+	return;
+
+	HandlePacket(packetData, packetSize);
+}
+
+void NetworkClient::SendInput(int steering, bool thrust, bool fire)
+{
+	if(!connected)        
+	return;
+}
+
+bool NetworkClient::IsConnected() const
+{
+	return connected;
+}
+
 void NetworkClient::HandlePacket(const uint8_t *data, size_t size)
 {
     NetworkSnapshot snapshot;
