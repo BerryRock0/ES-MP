79583faac8096f7bb7d016deb9ec4307b13c6644
BerryRock0 <mdenials128@gmail.com>
2026-09-20 20:03:43 +0000

    Update NetworkSession.h

--- source/NetworkSession.h
+++ source/NetworkSession.h
@@ -1,1 +1,44 @@
+// NetworkSession.h
+#pragma once
 
+#include "NetworkSnapshot.h"
+
+#include <cstdint>
+#include <functional>
+#include <string>
+
+class GameModel;
+
+class NetworkSession
+{
+public:
+    enum class State
+    {
+        Offline,
+        Connecting,
+        Connected,
+        Disconnecting
+    };
+
+    explicit NetworkSession(GameModel &game);
+    ~NetworkSession();
+
+    bool Connect(const std::string &host, uint16_t port);
+    void Disconnect();
+
+    void Update(double deltaTime);
+
+    void SendPlayerInput(
+        uint32_t buttons,
+        float thrust,
+        float turn);
+
+    State GetState() const;
+
+private:
+    class NetworkClient *client = nullptr;
+    GameModel &game;
+    State state = State::Offline;
+
+    void HandleSnapshot(const NetworkSnapshot &snapshot);
+};
