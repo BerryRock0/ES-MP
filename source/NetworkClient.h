d6c428aad98929cf71794109eb5736481c5d3638
BerryRock0 <mdenials128@gmail.com>
2026-09-20 20:11:58 +0000

    Update NetworkClient.h

--- source/NetworkClient.h
+++ source/NetworkClient.h
@@ -1,1 +1,19 @@
+class NetworkClient
+{
+public:
+    bool Connect(const std::string &host, uint16_t port);
+    void Disconnect();
+    void Poll();
 
+    void SendInput(
+        int steering,
+        bool thrust,
+        bool fire);
+
+    bool IsConnected() const;
+
+private:
+    // UDP/TCP socket
+    // receive buffer
+    // connection state
+};
