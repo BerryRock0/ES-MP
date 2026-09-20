5d54df3df86d9e86eb5e4197a8857750cc1fc755
BerryRock0 <mdenials128@gmail.com>
2026-09-20 20:13:16 +0000

    Update NetworkSnapshot.h

--- source/NetworkSnapshot.h
+++ source/NetworkSnapshot.h
@@ -1,1 +1,23 @@
+#pragma once
 
+#include <cstdint>
+#include <vector>
+
+struct NetworkShipState
+{
+    uint32_t networkId = 0;
+
+    double x = 0.;
+    double y = 0.;
+    double vx = 0.;
+    double vy = 0.;
+    double angle = 0.;
+
+    int hull = 0;
+};
+
+struct NetworkSnapshot
+{
+    uint32_t tick = 0;
+    std::vector<NetworkShipState> ships;
+};
