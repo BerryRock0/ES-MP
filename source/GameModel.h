ad4ab6553317ae3225c23b36fe486a01ee34c510
BerryRock0 <mdenials128@gmail.com>
2026-09-20 20:14:03 +0000

    Create GameModel.h

---
+++ source/GameModel.h
@@ -0,0 +1,9 @@
+class GameModel
+{
+public:
+    void ApplyNetworkSnapshot(const NetworkSnapshot &snapshot);
+    void UpdateNetworkInterpolation(double deltaTime);
+
+private:
+    // References or access to existing game objects.
+};
