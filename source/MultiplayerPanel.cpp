49f05e99ade8e4599aca1f7f7681a7880b83c009
BerryRock0 <mdenials128@gmail.com>
2026-09-20 21:11:26 +0000

    Update MultiplayerPanel.cpp

--- source/MultiplayerPanel.cpp
+++ source/MultiplayerPanel.cpp
@@ -1,17 +1,107 @@
 #include "MultiplayerPanel.h"
 
 #include "NetworkSession.h"
+#include "audio/Audio.h"
+#include "text/Clipboard.h"
+#include "Color.h"
+#include "Command.h"
+#include "text/DisplayText.h"
+#include "Endpoint.h"
+#include "shader/FillShader.h"
+#include "text/Font.h"
+#include "text/FontSet.h"
+#include "GameData.h"
+#include "Point.h"
+#include "Preferences.h"
+#include "Screen.h"
+#include "shift.h"
+#include "image/Sprite.h"
+#include "image/SpriteSet.h"
+#include "shader/SpriteShader.h"
+#include "TextArea.h"
+#include "UI.h"
 
 #include <cctype>
 #include <cstdint>
 #include <stdexcept>
+#include <cmath>
+#include <utility>
 
 MultiplayerPanel::MultiplayerPanel()
 {}
 
 void MultiplayerPanel::Draw()
 {
-    // Use the game's existing panel/font/button drawing code here.
+	DrawBackdrop();
+
+	const Sprite *top = SpriteSet::Get(isWide ? "ui/dialog top wide" : "ui/dialog top");
+	const Sprite *middle = SpriteSet::Get(isWide ? "ui/dialog middle wide" : "ui/dialog middle");
+	const Sprite *bottom = SpriteSet::Get(isWide ? "ui/dialog bottom wide" : "ui/dialog bottom");
+	const Sprite *cancel = SpriteSet::Get("ui/dialog cancel");
+	const Sprite *thirdButtonSprite = SpriteSet::Get("ui/wide button");
+
+	// Get the position of the top of this dialog, and of the input.
+	Point pos(0., (top->Height() + extensionCount * middle->Height() + bottom->Height()) * -.5);
+	Point inputPos = Point(0., -(cancel->Height() + INPUT_HEIGHT)) - pos;
+
+	// Draw the top section of the dialog box.
+	pos.Y() += top->Height() * .5;
+	SpriteShader::Draw(top, pos);
+	pos.Y() += top->Height() * .5;
+
+	// The middle section is duplicated depending on how long the text is.
+	for(int i = 0; i < extensionCount; ++i)
+	{
+		pos.Y() += middle->Height() * .5;
+		SpriteShader::Draw(middle, pos);
+		pos.Y() += middle->Height() * .5;
+	}
+
+	// Draw the bottom section.
+	const Font &font = FontSet::Get(Preferences::GetFontSize());
+	pos.Y() += bottom->Height() * .5;
+	SpriteShader::Draw(bottom, pos);
+	pos.Y() += (bottom->Height() - cancel->Height()) * .5;
+
+	// Draw the buttons, including optionally the cancel button.
+	const Color &bright = *GameData::Colors().Get("bright");
+	const Color &dim = *GameData::Colors().Get("medium");
+	const Color &back = *GameData::Colors().Get("faint");
+	const Color &inactive = *GameData::Colors().Get("inactive");
+	okPos = pos + Point((top->Width() - RIGHT_MARGIN - cancel->Width()) * .5, 0.);
+	Point labelPos(okPos.X() - .5 * font.Width(okText), okPos.Y() - .5 * font.Height());
+	font.Draw(okText, labelPos, isOkDisabled ? inactive : (activeButton == 1 ? bright : dim));
+	if(canCancel)
+	{
+		cancelPos = pos + Point(okPos.X() - cancel->Width() + BUTTON_RIGHT_MARGIN, 0.);
+		SpriteShader::Draw(cancel, cancelPos);
+		labelPos = {cancelPos.X() - .5 * font.Width(cancelText), cancelPos.Y() - .5 * font.Height()};
+		font.Draw(cancelText, labelPos, activeButton == 2 ? bright : dim);
+
+		if(numButtons == 3)
+		{
+			// Third button, always the left-most button:
+			thirdPos = pos + Point(cancelPos.X() - (thirdButtonSprite->Width() + cancel->Width()) / 2 + BUTTON_RIGHT_MARGIN, 0.);
+			SpriteShader::Draw(thirdButtonSprite, thirdPos);
+			labelPos = {thirdPos.X() - .5 * font.Width(buttonThree.buttonLabel), thirdPos.Y() - .5 * font.Height()};
+			font.Draw(buttonThree.buttonLabel, labelPos, activeButton == 3 ? bright : dim);
+		}
+	}
+
+	// Draw the input, if any.
+	if(AcceptsInput())
+	{
+		FillShader::Fill(inputPos, Point(Width() - HORIZONTAL_PADDING, INPUT_HEIGHT), (flickerTime % 6 > 3) ? dim : back);
+		if(flickerTime)
+			--flickerTime;
+
+		Point stringPos(inputPos.X() - (Width() - HORIZONTAL_PADDING) * .5 + INPUT_LEFT_PADDING, inputPos.Y() - .5 * font.Height());
+		const auto inputText = DisplayText(input, {static_cast<int>(Width() - HORIZONTAL_PADDING - INPUT_HORIZONTAL_PADDING), Truncate::FRONT});
+		font.Draw(inputText, stringPos, bright);
+
+		Point barPos(stringPos.X() + font.FormattedWidth(inputText) + INPUT_TOP_PADDING, inputPos.Y());
+		FillShader::Fill(barPos, Point(1., INPUT_HEIGHT - INPUT_VERTICAL_PADDING), dim);
+	}
 }
 
 void MultiplayerPanel::HandleTextInput(const std::string &text)
