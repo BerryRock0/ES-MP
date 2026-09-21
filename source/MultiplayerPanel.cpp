1538e11427cc556660bfbed15a531e5860be0ff5
BerryRock0 <mdenials128@gmail.com>
2026-09-21 12:45:29 +0000

    Update MultiplayerPanel.cpp

--- source/MultiplayerPanel.cpp
+++ source/MultiplayerPanel.cpp
@@ -33,7 +33,6 @@
 
 MultiplayerPanel::MultiplayerPanel(const MultiplayerInit &init) : Panel(), networkSession(), address(), port(), addressFocused(true), portFocused(false), connecting(false)
 {
-	MultiplayerInit init;
 	init.message = "Connect to server";
 	init.canCancel = true;
 	init.initialValue.clear();
